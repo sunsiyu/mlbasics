7b4bc9ae514173725a308cd13973e3d02b53776c
Siyu Sun <sunsiyu.tud@gmail.com>
2015-10-29 17:48:39 +0100

    add normalization functions.

--- reg.R
+++ reg.R
@@ -1,4 +1,25 @@
-g <- function(z){
-  
+g <- function(z) {
   return(1/(1+exp(-z)))
 }
+
+
+minmaxnorm <- function(x) {
+  
+  stopifnot(is.numeric(x))
+  
+  if (identical(range(x, na.rm = T)[1], range(x, na.rm = T)[2]))
+    return(x/x)
+  else
+    return((x-min(x, na.rm=T)) / (max(x, na.rm=T) - min(x, na.rm=T)) )
+}
+
+zscorenorm <- function(x) {
+  
+  stopifnot(is.numeric(x))
+  
+  if (sd(x, na.rm = T) == 0)
+    return(x/x)
+  else
+    return((x-mean(x, na.rm = T)) / sd(x, na.rm = T))
+}
+  
