313510db1016321a13131384e2c6516264d9331a
Siyu Sun <sunsiyu.tud@gmail.com>
2015-11-15 13:10:33 +0100

    add preProcess with caret.

--- wage.R
+++ wage.R
@@ -2,6 +2,7 @@
 library(ISLR) # dataset package
 library(ggplot2)
 library(caret)
+library(splines)
 data(Wage)
 summary(Wage)
 str(Wage)
@@ -17,3 +18,31 @@ featurePlot(x = training[, c("age", "jobclass", "education")],
 # making factors
 cutWage <- Hmisc::cut2(training$wage, g = 3)
 table(cutWage)
+
+# Covariate Creation
+# Basic idea - convert factor variables to indicator variables
+table(training$jobclass)
+dummies <- dummyVars(wage ~ jobclass, data = training)
+head(predict(dummies, newdata = training))
+
+# Remove zero covariates (have one unique value, identify near 0 variance predictor)
+nsv <- nearZeroVar(training, saveMetrics = TRUE)
+
+# Spline Basis
+bsBasis <- splines::bs(training$age, df=3)
+# col.1 original par
+# col.2 square
+# col.3 cube
+
+# Fitting curves with spline
+lm1 <- lm(wage ~ bsBasis, data = training)
+plot(training$age, training$wage, pch = 19, cex = 0.5)
+points(training$age, predict(lm1, newdata = training), col="red", pch = 19, cex = 0.5)
+
+p <- ggplot(training, aes(age, wage))
+p + geom_point() + geom_line(aes(y=predict(lm1, newdata = training)), color="red")
+
+
+
+
+
